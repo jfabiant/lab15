1d7910615400fd40e09f758c1102708f0c7a7b54
Camavilca <orlando.camavilca@tecsup.edu.pe>
2019-11-26 15:46:21 -0500

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+    var socket = io();
+    $('#formulario').submit(function (e) {
+        e.preventDefault();
+        var data = {
+            _id: $('#_id').val(),
+            first_name: $('#first_name').val(),
+            last_name: $('#last_name').val(),
+            timezone: $('#timezone').val(),
+            locale: $('#locale').val(),
+            profile_pic: $('#profile_pic').val()
+        };
+        console.log(data);
+        if (data._id == '') {
+            $("#_id").focus();
+            return alert("Debe ingreser un ID!");
+        }
+        if (data.first_name == '') {
+            $("#first_name").focus();
+            return alert("Debe ingreser un nombre!");
+        }
+        socket.emit('crear', data);
+        $('#formulario').trigger('reset');
+        return true;
+    });
 });
--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
+const io = require('socket.io')(http);
 
 app.set('view engine', 'jade');
 
 app.use('/static', express.static('public'));
+
+io.on('connection', function (socket) {
+    console.log('Usuario conectado');
+    socket.on('disconnect', function () {
+        console.log('Usuario desconectado');
+    });
+});
 
 app.get('/', (req, res) => {
     res.render('main');
